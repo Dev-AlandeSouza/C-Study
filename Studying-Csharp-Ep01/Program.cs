3a74fb5e06621bdc4ea35b6347f5da826e48af60
Dev-AlandeSouza <alaan.correia.08@gmail.com>
2025-04-07 01:12:57 -0300

    initializing in C#

---
+++ Studying-Csharp-Ep01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Studying_Csharp_Ep01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }  
+}
---
+++ Studying-Csharp-Ep02/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace StudyingCsharpEp02;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // This is a comment
+
+        /*
+        This is a multi-line comment
+        It can span multiple lines
+        */
+
+        string myName = "Alan de Souza"; // String variable
+        int myAge = 21; // Integer variable
+        double myHeight = 1.73; // Double variable
+        bool student = true; // Boolean variable
+        char myInitial = 'A'; // Character variable
+
+        Console.WriteLine("Meu nome é " + myName + ", tenho " + myAge + " anos, minha altura é " + myHeight + "m, sou estudante: " + student + ", minha inicial é " + myInitial);
+        // String interpolation (C# 6.0 and later)
+    }
+}
